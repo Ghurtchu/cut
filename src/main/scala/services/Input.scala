2825a57ea94fa9544f00e385bdb3b00d3478f06f
Anzori (Nika) Gurchumelia <aghurtchumelia@Anzoris-MBP.home>
2023-08-26 19:30:32 +0200

    rename to Input

--- src/main/scala/services/Input.scala
+++ src/main/scala/services/Input.scala
@@ -3,17 +3,17 @@ package services
 import java.nio.file.{Files, Path}
 import scala.util.Try
 
-trait LoadInput {
+trait Input {
   def load: String
 }
 
-object LoadInput {
-  def fromFile(filename: String): LoadInput = new LoadInput {
+object Input {
+  def fromFile(filename: String): Input = new Input {
     override def load: String =
       Try(Files readString (Path of filename))
         .getOrElse(throw new RuntimeException("File does not exist"))
   }
-  def fromStdIn: LoadInput = new LoadInput {
+  def fromStdIn: Input = new Input {
     override def load: String =
       scala.io.Source.stdin.getLines
         .mkString("\n")
